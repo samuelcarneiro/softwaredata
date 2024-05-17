d1d17f8f69fefcfd5b388486f3e949b08f4006e8
Samuel Carneiro <samuelcarneiro.al@gmail.com>
2024-05-17 10:50:00 -0300

    Ajustes no menu mobile e icones do menu.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,12 @@
 function menuShow() {
-    let menuMobile = document.querySelector('.mobile-menu');
-    if (menuMobile.classList.contains('open')) {
-        menuMobile.classList.remove('open');
-        document.querySelector('.icon-menu').src="./assets/img/menu_white_36dp.svg";
-    } else {
-        menuMobile.classList.add('open');
-        document.querySelector('.icon-menu').src="./assets/img/close_white_36dp.svg";
-    }
+  let menuMobile = document.querySelector('.mobile-menu');
+  if (menuMobile.classList.contains('open')) {
+      menuMobile.classList.remove('open');
+      document.querySelector('.icon-menu').src="./assets/img/menu_soft_36dp.png";
+  } else {
+      menuMobile.classList.add('open');
+      document.querySelector('.icon-menu').src="./assets/img/close_soft_36dp.png";
+  }
 }
 
 /* slider */
